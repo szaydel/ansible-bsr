761406b9385d8a723e5c1b88db7873f3565b3e4e
Sam Zaydel <szaydel@gmail.com>
2022-08-17 06:08:34 -0700

    Add classes for managing access control lists and SMB shares

---
+++ module_utils/netacl.py
@@ -0,0 +1,278 @@
+import ipaddress
+import re
+from typing import List, Tuple
+import unittest
+
+
+IP4_RE = re.compile(
+    r"(\b25[0-5]|\b2[0-4][0-9]|\b[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}"
+)
+
+
+class InvalidAddressSpecification(Exception):
+    pass
+
+
+class IPNetHostList:
+    """Provides a class with convenience methods over the list of nets and hosts to be included in the ACL."""
+
+    def __init__(self, addrs: str):
+        self._addrs = addrs
+        self._addrs_list = self._list_from_addrs_string()
+        self._hosts, self._nets, self._others = self._split_nets_and_hosts()
+
+    @property
+    def hosts(self):
+        return self._hosts
+
+    @property
+    def nets(self):
+        return self._nets
+
+    @property
+    def others(self):
+        return self._others
+
+    def __str__(self):
+        addrs = []
+        for elem in self.nets + self.hosts:
+            addrs.append("@" + str(elem))
+        addrs += self._others
+        return ":".join(addrs)
+
+    def __len__(self):
+        return len(self.hosts) + len(self.nets)
+
+    def _validate_addr(self, host_or_net_addr: str):
+        # If there is a slash '/' in the address, it is a network.
+        if "/" in host_or_net_addr:  # network address case
+            try:
+                # FIXME: We probably should be doing more validation here, but
+                # for now this is sufficient to know that the thing is at least
+                # valid.
+                _ = ipaddress.IPv4Network(host_or_net_addr)
+            except ipaddress.NetmaskValueError as err:
+                raise InvalidAddressSpecification(err.args[0])
+            except ValueError as err:
+                raise InvalidAddressSpecification(err.args[0])
+        else:  # host address case
+            try:
+                addr = ipaddress.IPv4Address(host_or_net_addr)
+                # Loopback address is not legal in this context
+                if addr.is_loopback:
+                    raise InvalidAddressSpecification(
+                        f"Local address {host_or_net_addr} not allowed"
+                    )
+            except ipaddress.AddressValueError as err:
+                raise InvalidAddressSpecification(err.args[0])
+
+    def _list_from_addrs_string(self) -> Tuple[str]:
+        addrs_list = []
+        tokens = self._addrs.split(":")
+        if not tokens:
+            return tuple()
+        # Validate that what we have looks like valid IP addresses/ranges.
+        for token in tokens:
+            if token == "":
+                continue
+            # IP addresses and network ranges are prefixed with '@'.
+            elif token[0] == "@":
+                # If not valid, this will raise an exception.
+                self._validate_addr(token[1:])
+                addrs_list.append(token)
+            # This is a IPv4 address, but apparently lacks the '@' prefix.
+            elif IP4_RE.match(token):
+                self._validate_addr(token)
+                addrs_list.append("@" + token)
+            # Anything else, i.e. hostname, FQDN, etc.
+            else:
+                addrs_list.append(token)
+        return tuple(addrs_list)
+
+    def _split_nets_and_hosts(
+        self,
+    ) -> Tuple[Tuple[ipaddress.IPv4Address], Tuple[ipaddress.IPv4Network], Tuple[str]]:
+        addr_list = self._list_from_addrs_string()
+        hosts: List[ipaddress.IPv4Address] = list()
+        nets: List[ipaddress.IPv4Network] = list()
+        other: List[str] = list()
+        for elem in addr_list:
+            if elem == "":
+                continue
+            # if not elem[0] == "@":
+            #     continue
+            # All of the IP elements should have the '@' prefix.
+            if elem[0].isalpha():
+                other.append(elem)
+            elif "/" in elem:
+                n = ipaddress.IPv4Network(elem[1:])
+                nets.append(n)
+            else:
+                h = ipaddress.IPv4Address(elem[1:])
+                hosts.append(h)
+        return tuple(hosts), tuple(nets), tuple(other)
+
+
+def raise_on_conflict(ro_list: IPNetHostList, rw_list: IPNetHostList):
+    """Raises an exception if there is a conflict between the two lists. These lists are mutually exclusive.
+
+    Args:
+        ro_list (IPNetHostList): List of networks and hosts in the Read-Only ACL
+        rw_list (IPNetHostList): List of networks and hosts in the Read-Write ACL
+
+    Raises:
+        InvalidAddressSpecification: Any conflict, such as overlap between ACLs leads to this exception being raised.
+    """
+    ro_hosts = ro_list.hosts
+    ro_nets = ro_list.nets
+    ro_others = ro_list.others
+    rw_hosts = rw_list.hosts
+    rw_nets = rw_list.nets
+    rw_others = rw_list.others
+    # First, make sure we do not have any host addresses in both groups.
+    for ro_host in ro_hosts:
+        for rw_host in rw_hosts:
+            if ro_host == rw_host:
+                raise InvalidAddressSpecification(
+                    f"Found host address {str(ro_host)} in read/write and read-only lists"
+                )
+    # Check network overlaps.
+    for ro_net in ro_nets:
+        for rw_net in rw_nets:
+            if ro_net.overlaps(rw_net):
+                raise InvalidAddressSpecification(
+                    f"Found network address {str(ro_net)} in read/write and read-only lists"
+                )
+    # Check for any host addresses that may belong to network in the other
+    # list.
+    for ro_host in ro_hosts:
+        for rw_net in rw_nets:
+            if ro_host in rw_net:
+                raise InvalidAddressSpecification(
+                    f"Found host address {str(ro_host)} in read-only list belonging to network address {rw_net} in read/write list"
+                )
+    for rw_host in rw_hosts:
+        for ro_net in ro_nets:
+            if rw_host in ro_net:
+                raise InvalidAddressSpecification(
+                    f"Found host address {str(rw_host)} in read/write list belonging to network address {ro_net} in read-only list"
+                )
+    for ro_other in ro_others:
+        if ro_other in rw_others:
+            raise InvalidAddressSpecification(
+                f"Found name {ro_other} in read-only and read/write lists"
+            )
+
+
+class TestIPNetHostList(unittest.TestCase):
+    def test_list_from_addrs_string_is_correct(self):
+        addrs = IPNetHostList(
+            "10.100.10.0/24:10.2.0.0/16:192.168.100.1:192.168.100.5:alpha.beta.com"
+        )
+        self.assertEqual(
+            addrs._addrs_list,
+            (
+                "@10.100.10.0/24",
+                "@10.2.0.0/16",
+                "@192.168.100.1",
+                "@192.168.100.5",
+                "alpha.beta.com",
+            ),
+        )
+
+    def test_list_creation_is_correct(self):
+        test_list = IPNetHostList(
+            "10.100.10.0/24:10.2.0.0/16:192.168.100.1:192.168.100.5"
+        )
+
+        self.assertEqual(
+            test_list.hosts,
+            (
+                ipaddress.IPv4Address("192.168.100.1"),
+                ipaddress.IPv4Address("192.168.100.5"),
+            ),
+        )
+        self.assertEqual(
+            test_list.nets,
+            (
+                ipaddress.IPv4Network("10.100.10.0/24"),
+                ipaddress.IPv4Network("10.2.0.0/16"),
+            ),
+        )
+
+    def test_net_has_host_bits(self):
+        with self.assertRaises(InvalidAddressSpecification) as e:
+            _ = IPNetHostList("10.1.2.0/16")
+        self.assertEqual(
+            e.exception.args[0],
+            "10.1.2.0/16 has host bits set",
+        )
+
+    def test_validation_raises_exception(self):
+        test_ro_list = IPNetHostList(
+            "10.100.10.0/24:10.2.0.0/16:192.168.100.1:192.168.100.5"
+        )
+        test_rw_list = IPNetHostList(
+            "10.100.10.0/24:10.2.0.0/16:192.168.100.1:192.168.100.5"
+        )
+        with self.assertRaises(InvalidAddressSpecification) as e:
+            raise_on_conflict(test_ro_list, test_rw_list)
+        self.assertEqual(
+            e.exception.args[0],
+            "Found host address 192.168.100.1 in read/write and read-only lists",
+        )
+
+        test_ro_list = IPNetHostList("10.100.10.5")
+        test_rw_list = IPNetHostList("10.100.10.0/24")
+
+        with self.assertRaises(InvalidAddressSpecification) as e:
+            raise_on_conflict(test_ro_list, test_rw_list)
+        self.assertEqual(
+            e.exception.args[0],
+            "Found host address 10.100.10.5 in read-only list belonging to network address 10.100.10.0/24 in read/write list",
+        )
+
+        test_rw_list = IPNetHostList("10.100.0.0/16")
+        test_rw_list = IPNetHostList("10.100.10.5")
+
+        with self.assertRaises(InvalidAddressSpecification) as e:
+            raise_on_conflict(test_ro_list, test_rw_list)
+        self.assertEqual(
+            e.exception.args[0],
+            "Found host address 10.100.10.5 in read/write and read-only lists",
+        )
+
+    def test_hosts_property_correct(self):
+        test_list = IPNetHostList(
+            "10.100.10.0/24:10.2.0.0/16:192.168.100.1:192.168.100.5"
+        )
+        self.assertEqual(
+            test_list.hosts,
+            (
+                ipaddress.IPv4Address("192.168.100.1"),
+                ipaddress.IPv4Address("192.168.100.5"),
+            ),
+        )
+
+    def test_nets_property_correct(self):
+        test_list = IPNetHostList(
+            "10.100.10.0/24:10.2.0.0/16:192.168.100.1:192.168.100.5"
+        )
+        self.assertEqual(
+            test_list.nets,
+            (
+                (
+                    ipaddress.IPv4Network("10.100.10.0/24"),
+                    ipaddress.IPv4Network("10.2.0.0/16"),
+                )
+            ),
+        )
+
+    def test_others_property_correct(self):
+        test_list = IPNetHostList(
+            "10.100.10.0/24:10.2.0.0/16:192.168.100.1:192.168.100.5:alpha.beta.com:beta.alpha.com"
+        )
+        self.assertEqual(
+            test_list.others,
+            ("alpha.beta.com", "beta.alpha.com"),
+        )
---
+++ module_utils/smb.py
@@ -0,0 +1,227 @@
+from dataclasses import dataclass
+import unittest
+
+from .netacl import IPNetHostList
+from .netacl import raise_on_conflict
+
+
+# 11 39:18 0.027879 [POST-4] 10.2.22.87> /usr/sbin/zfs set sharesmb=name=a,abe=true,csc=disabled,encrypt=required,ro=@10.255.4.0/24:@10.255.7.0/24,rw=@10.255.2.0/24:@10.255.5.0/24,none=@10.255.2.3:@10.255.2.4 p01/global/ahttps://10.2.22.87:8443/internal/v1/shell/run{"Command":"/usr/sbin/zfs","Args":"set sharesmb=name=a,abe=true,csc=disabled,encrypt=required,ro=@10.255.4.0/24:@10.255.7.0/24,rw=@10.255.2.0/24:@10.255.5.0/24,none=@10.255.2.3:@10.255.2.4 p01/global/a","UseShell":false,"IsQuery":false,"ActionId":"22ff7e60-91ec-4445-ab03-7ad519d6e382"}
+
+
+@dataclass
+class SMBShare:
+    name: str
+    abe: bool  # Access Based Enumeration
+    csc: bool  # Client-side Caching (enabling this breaks UB)
+    encrypt: str
+    read_only: str
+    read_write: str
+    none: str
+    ub: bool
+
+    @property
+    def csc_setting(self):
+        legal_values = ("manual", "auto", "vdo", "disabled")
+        if self.csc in legal_values:
+            return self.csc
+        raise ValueError(f"Invalid value for csc option: {self.csc}")
+
+    @property
+    def encrypt_setting(self):
+        legal_values = ("enabled", "disabled", "required")
+        if self.encrypt in legal_values:
+            return self.encrypt
+
+        raise ValueError(f"Invalid value for encrypt option: {self.encrypt}")
+
+    @property
+    def abe_setting(self):
+        if isinstance(self.abe, bool):
+            return self.abe
+        raise TypeError(f"Invalid type for abe option: {self.abe}")
+
+    @property
+    def ub_setting(self):
+        if isinstance(self.ub, bool):
+            return self.ub
+        raise TypeError(f"Invalid type for racktop:ub option: {self.abe}")
+
+    def __str__(self):
+        parts = []
+        parts.append(f"name={self.name}")
+        parts.append(f"abe={'true' if self.abe_setting else 'false'}")
+        parts.append(f"csc={self.csc_setting}")
+        parts.append(f"encrypt={self.encrypt_setting}")
+        if self.read_only_list() is not None:
+            parts.append(f"ro={self.fmt_read_only_list}")
+        if self.read_write_list() is not None:
+            parts.append(f"rw={self.fmt_read_write_list}")
+        if self.none_list() is not None:
+            parts.append(f"none={self.fmt_none_list}")
+        return (
+            "sharesmb="
+            + ",".join(parts)
+            + " "
+            + f"racktop:ub={'on' if self.ub_setting else 'off'}"
+        )
+
+    @property
+    def fmt_read_write_list(self):
+        """Formats read/write list and converts into a string usable with zfs set command.
+
+        Returns:
+            str: Read/write list as a string.
+        """
+        return str(self.read_write_list())
+
+    @property
+    def fmt_read_only_list(self):
+        """Formats read-only list and converts into a string usable with zfs set command.
+
+        Returns:
+            str: Read-only list as a string.
+        """
+        return str(self.read_only_list())
+
+    @property
+    def fmt_none_list(self):
+        """Formats other list and converts into a string usable with zfs set command.
+
+        Returns:
+            str: Other list as a string.
+        """
+        return str(self.none_list())
+
+    def validate_access_lists(self):
+        """
+        Validates that the configuration is sane and will be accepted. Triggers
+        an exception if the configuration is not appropriate.
+        """
+        raise_on_conflict(self.read_only_list(), self.read_write_list())
+
+    def read_only_list(self):
+        """Coverts a given string to an read-only IPNetHostList.
+
+        Returns:
+            IPNetHostList: Read-only list represented as native objects.
+        """
+        if self.read_only:
+            return IPNetHostList(self.read_only)
+
+    def read_write_list(self):
+        """Coverts a given string to an read/write IPNetHostList.
+
+        Returns:
+            IPNetHostList: Read/write list represented as native objects.
+        """
+        if self.read_write:
+            return IPNetHostList(self.read_write)
+
+    def none_list(self):
+        """Coverts a given string to an none IPNetHostList.
+
+        Returns:
+            IPNetHostList: None list represented as native objects.
+        """
+        if self.none:
+            return IPNetHostList(self.none)
+
+
+class TestSMBShare(unittest.TestCase):
+    def test_smb_share_setting_is_correct(self):
+
+        test_cases = (
+            {
+                "params": SMBShare(
+                    name="test",
+                    abe=True,
+                    csc="disabled",
+                    encrypt="required",
+                    read_only="10.0.0.0/8:1.2.3.4:10.1.0.0/16:12.13.14.0/24:foobar.alpha.com",
+                    read_write="12.13.15.3:5.6.0.0/24",
+                    none="",
+                    ub=True,
+                ),
+                "want": "sharesmb=name=test,abe=true,csc=disabled,encrypt=required,ro=@10.0.0.0/8:@10.1.0.0/16:@12.13.14.0/24:@1.2.3.4:foobar.alpha.com,rw=@5.6.0.0/24:@12.13.15.3 racktop:ub=on",
+            },
+            {
+                "params": SMBShare(
+                    name="test",
+                    abe=True,
+                    csc="disabled",
+                    encrypt="required",
+                    read_only="10.0.0.0/8:1.2.3.4:10.1.0.0/16:12.13.14.0/24:foobar.alpha.com",
+                    read_write="12.13.15.3:5.6.0.0/24",
+                    none="10.0.0.0/8",
+                    ub=True,
+                ),
+                "want": "sharesmb=name=test,abe=true,csc=disabled,encrypt=required,ro=@10.0.0.0/8:@10.1.0.0/16:@12.13.14.0/24:@1.2.3.4:foobar.alpha.com,rw=@5.6.0.0/24:@12.13.15.3,none=@10.0.0.0/8 racktop:ub=on",
+            },
+            {
+                "params": SMBShare(
+                    name="test",
+                    abe=True,
+                    csc="disabled",
+                    encrypt="required",
+                    read_only="10.0.0.0/8:1.2.3.4:10.1.0.0/16:12.13.14.0/24:foobar.alpha.com",
+                    read_write="12.13.15.3:5.6.0.0/24",
+                    none="10.0.0.0/8:alpha.beta.com",
+                    ub=False,
+                ),
+                "want": "sharesmb=name=test,abe=true,csc=disabled,encrypt=required,ro=@10.0.0.0/8:@10.1.0.0/16:@12.13.14.0/24:@1.2.3.4:foobar.alpha.com,rw=@5.6.0.0/24:@12.13.15.3,none=@10.0.0.0/8:alpha.beta.com racktop:ub=off",
+            },
+        )
+        for case in test_cases:
+            case["params"].validate_access_lists()
+            actual = str(case["params"])
+            self.assertEqual(actual, case["want"])
+
+    def test_smb_share_invalid_settings(self):
+        test_cases = (
+            {
+                "params": SMBShare(
+                    name="test",
+                    abe=True,
+                    csc="disabled",
+                    encrypt="bogus",
+                    read_only="10.0.0.0/8:1.2.3.4:10.1.0.0/16:12.13.14.0/24:foobar.alpha.com",
+                    read_write="12.13.15.3:5.6.0.0/24",
+                    none="",
+                    ub=True,
+                ),
+                "exception": ValueError,
+            },
+            {
+                "params": SMBShare(
+                    name="test",
+                    abe=True,
+                    csc="bogus",
+                    encrypt="required",
+                    read_only="10.0.0.0/8:1.2.3.4:10.1.0.0/16:12.13.14.0/24:foobar.alpha.com",
+                    read_write="12.13.15.3:5.6.0.0/24",
+                    none="10.0.0.0/8",
+                    ub=True,
+                ),
+                "exception": ValueError,
+            },
+            {
+                "params": SMBShare(
+                    name="test",
+                    abe=None,
+                    csc="disabled",
+                    encrypt="required",
+                    read_only="10.0.0.0/8:1.2.3.4:10.1.0.0/16:12.13.14.0/24:foobar.alpha.com",
+                    read_write="12.13.15.3:5.6.0.0/24",
+                    none="10.0.0.0/8:alpha.beta.com",
+                    ub=False,
+                ),
+                "exception": TypeError,
+            },
+        )
+        for case in test_cases:
+            with self.assertRaises(case["exception"]) as e:
+                case["params"].validate_access_lists()
+                _ = str(case["params"])
+            self.assertRegex(
+                e.exception.args[0], r"Invalid (type|value) for \S+ option: \S+"
+            )
